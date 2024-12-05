b9fc8297d3dc0cac693b8eed8800e46df72d1130
SahilKumarIN <rudrasahil3@gmail.com>
2024-12-05 21:30:07 +0530

    initial commit

---
+++ components/Timeline.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import React from "react";
+import { Timeline } from "@/components/ui/timeline";
+
+export function TimelineDemo() {
+  const data = [
+    {
+      title: "2022-2026",
+      content: (
+        <div>
+          <p className="text-neutral-800 font-extrabold dark:text-neutral-200 text-2xl md:text-sm mb-8">
+            Bengal College of Engineering & Technology , Durgapur
+          </p>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            B.Tech , CSE
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="https://assets.aceternity.com/templates/startup-1.webp"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "2007-2022",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-2xl md:text-sm font-bold mb-8">
+            St. Teresa's School , Bhagalpur
+          </p>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Completed secondary and senior secondary education with PCM +
+            Computer Science.
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="https://assets.aceternity.com/pro/hero-sections.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className="w-full">
+      <Timeline data={data} />
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        
+        "hostname": "assets.aceternity.com"
+      },
+      {
+        "hostname": "images.unsplash.com"
+      },
+      {
+        "hostname": "images.pexels.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
